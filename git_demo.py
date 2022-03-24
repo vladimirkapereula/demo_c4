65ee2bd4d98c59ac782ce1bbfb45cdda261c771e
vladimirkapereula <vpereula@gmail.com>
2022-03-24 10:23:18 -0700

    extension for the file get_demo updated from .txt to .py

---
+++ git_demo.py
@@ -0,0 +1,5 @@
+#it is a git-demo.py file for lab03
+print('Commit 1:Git is a version controls technology installed on local machine!')
+print('Commit 2:GitHub is an online platform to host Git Repositories')
+print('Commit 3:Changes in repository being recorded in README.md file')
+print('Commit 4:Extension for the file git_demo changed from .text to .py')
